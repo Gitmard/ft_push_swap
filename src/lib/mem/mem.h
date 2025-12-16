b78741ae4f89110a83b94300f109ca4506265a3f
Victor Quetier <vquetier@z4r11p1.42lyon.fr>
2025-12-16 15:36:53 +0100

    feat: lib/mem

---
+++ src/lib/mem/mem.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mem.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vquetier <vquetier@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/16 15:33:11 by vquetier          #+#    #+#             */
+/*   Updated: 2025/12/16 15:35:24 by vquetier         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MEM_H
+# define MEM_H
+
+# include "mem_defines.h"
+
+void	ft_memset(void *ptr, uint8_t b, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+
+#endif

f1c22a4412e9a46f7e20134bb02beb69808d711a
smenard <floogles71@gmail.com>
2025-12-16 15:49:06 +0100

    feat: use ft_calloc in ft_lstnew

--- src/lib/list/ft_lstnew.c
+++ src/lib/list/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 13:39:58 by smenard           #+#    #+#             */
-/*   Updated: 2025/12/16 15:14:44 by smenard          ###   ########.fr       */
+/*   Updated: 2025/12/16 15:48:17 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,14 @@ t_list	*ft_lstnew(int value, size_t *size)
 {
 	t_list	*new;
 
-	// TODO: use ft_calloc
-	new = malloc(sizeof(t_list));
+	new = ft_calloc(1, sizeof(t_list));
 	if (!new)
 		return (NULL);
 	new->value = value;
 	if (size)
 		new->size = size;
 	else
-		new->size = malloc(sizeof(size_t));
+		new->size = ft_calloc(1, sizeof(size_t));
 	if (!new->size)
 		return (ft_lstclear(new));
 	return (new);

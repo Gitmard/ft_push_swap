305ee23831e3ae984c0fe01d9e28e1a3086fed43
smenard <floogles71@gmail.com>
2025-12-18 17:09:16 +0100

    refacto: norm

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 12:55:53 by vquetier          #+#    #+#             */
-/*   Updated: 2025/12/18 17:06:41 by smenard          ###   ########.fr       */
+/*   Updated: 2025/12/18 17:08:49 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,13 @@ int	main(int ac, char **av)
 	}
 	curr = args->stacks->stack_a->head;
 	__builtin_printf("stack_a :{head: %p, tail: %p, size: %zu}\n",
-			args->stacks->stack_a->head, args->stacks->stack_a->tail,
-			args->stacks->stack_a->size);
+		args->stacks->stack_a->head,
+		args->stacks->stack_a->tail,
+		args->stacks->stack_a->size);
 	while (curr)
 	{
 		__builtin_printf("%p: {next: %p, prec: %p, val: %d}\n", curr,
-				curr->next, curr->prev, curr->value);
+			curr->next, curr->prev, curr->value);
 		curr = curr->next;
 	}
 	free_args(args, FREE_ARGS_ALL);

ce287f1c6b3536bef58b058e7bde55275792f49b
smenard <floogles71@gmail.com>
2026-01-06 15:05:44 +0100

    chore: nvim env files

--- src/algo/algo_utils/algo_utils.c
+++ src/algo/algo_utils/algo_utils.c
@@ -12,44 +12,38 @@
 
 #include "algo_utils_defines.h"
 
-void	bubble_sort(int *tab, int size)
-{
-	int	i;
-	int	tmp;
+void bubble_sort(int *tab, int size) {
+  int i;
+  int tmp;
 
-	while (size > 1)
-	{
-		i = 0;
-		while (i < size - 1)
-		{
-			if (tab[i] > tab[i + 1])
-			{
-				tmp = tab[i];
-				tab[i] = tab[i + 1];
-				tab[i + 1] = tmp;
-			}
-			i++;
-		}
-		size--;
-	}
+  while (size > 1) {
+    i = 0;
+    while (i < size - 1) {
+      if (tab[i] > tab[i + 1]) {
+        tmp = tab[i];
+        tab[i] = tab[i + 1];
+        tab[i + 1] = tmp;
+      }
+      i++;
+    }
+    size--;
+  }
 }
 
-int	*create_sorted_array(t_stacks *stacks)
-{
-	t_list	*iter;
-	int		*tab;
-	int		i;
+int *create_sorted_array(t_stacks *stacks) {
+  t_list *iter;
+  int *tab;
+  int i;
 
-	iter = stacks->stack_a->head;
-	tab = malloc(sizeof(int) * stacks->stack_a->size);
-	if (!tab)
-		return (NULL);
-	i = 0;
-	while (iter)
-	{
-		tab[i] = iter->value;
-		i++;
-		iter = iter->next;
-	}
-	bubble_sort(tab, stacks->stack_a->size);
+  iter = stacks->stack_a->head;
+  tab = malloc(sizeof(int) * stacks->stack_a->size);
+  if (!tab)
+    return (NULL);
+  i = 0;
+  while (iter) {
+    tab[i] = iter->value;
+    i++;
+    iter = iter->next;
+  }
+  bubble_sort(tab, stacks->stack_a->size);
 }
--- src/lib/printf/utils.c
+++ src/lib/printf/utils.c
@@ -12,64 +12,57 @@
 
 #include "printf_lib.h"
 
-size_t	ft_strlen(t_string str)
-{
-	size_t	len;
+size_t ft_strlen(t_string str) {
+  size_t len;
 
-	if (!str)
-		return (0);
-	len = 0;
-	while (str[len])
-		len++;
-	return (len);
+  if (!str)
+    return (0);
+  len = 0;
+  while (str[len])
+    len++;
+  return (len);
 }
 
-size_t	ft_strlen_until(t_string str, char stop)
-{
-	size_t	len;
+size_t ft_strlen_until(t_string str, char stop) {
+  size_t len;
 
-	if (!str)
-		return (0);
-	len = 0;
-	while (str[len] && str[len] != stop)
-		len++;
-	return (len);
+  if (!str)
+    return (0);
+  len = 0;
+  while (str[len] && str[len] != stop)
+    len++;
+  return (len);
 }
 
-uint64_t	ft_abs(int64_t num)
-{
-	if (num < 0)
-		return (-num);
-	return (num);
+uint64_t ft_abs(int64_t num) {
+  if (num < 0)
+    return (-num);
+  return (num);
 }
 
-char	*ft_strchr(t_string str, char c)
-{
-	while (*str)
-	{
-		if (*str == c)
-			return (str);
-		str++;
-	}
-	return (NULL);
+char *ft_strchr(t_string str, char c) {
+  while (*str) {
+    if (*str == c)
+      return (str);
+    str++;
+  }
+  return (NULL);
 }
 
-t_string	ft_strdup(t_string str)
-{
-	size_t		len;
-	t_string	dup;
-	size_t		i;
+t_string ft_strdup(t_string str) {
+  size_t len;
+  t_string dup;
+  size_t i;
 
-	len = ft_strlen(str);
-	i = 0;
-	dup = malloc((len + 1) * sizeof(char));
-	if (!dup)
-		return (NULL);
-	while (str[i])
-	{
-		dup[i] = str[i];
-		i++;
-	}
-	dup[i] = '\0';
-	return (dup);
+  len = ft_strlen(str);
+  i = 0;
+  dup = malloc((len + 1) * sizeof(char));
+  if (!dup)
+    return (NULL);
+  while (str[i]) {
+    dup[i] = str[i];
+    i++;
+  }
+  dup[i] = '\0';
+  return (dup);
 }

d4b0ab902c0949e342dd16f43d3df7257abb4a09
Victor Quetier <vquetier@z2r11p7.42lyon.fr>
2026-01-07 17:16:05 +0100

    feat: parse

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 16:39:30 by vquetier          #+#    #+#             */
-/*   Updated: 2026/01/07 16:08:05 by vquetier         ###   ########lyon.fr   */
+/*   Updated: 2026/01/07 17:15:31 by vquetier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,14 +70,72 @@ int	check_flags(t_stacks *stacks)
 	return (ERROR);
 }
 
+int	count_words(char *arg)
+{
+	bool	flag;
+	int		count;
+	int		i;
+
+	flag = true;
+	count = 0;
+	i = 0;
+	while (arg[i])
+	{
+		if (arg[i] == ' ')
+			flag = true;
+		else if (flag)
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+int	count_full_words(int ac, char **av)
+{
+	int	words_count;
+	int	i;
+
+	words_count = 1;
+	i = 1;
+	while (i < ac)
+	{
+		words_count += count_words(av[i]);
+		i++;
+	}
+	return (words_count);
+}
+
+int	normalise_args(int &ac, char ***av, int &n_count)
+{
+	int		words_count;
+	char	**split;
+	bool	flag;
+	int		i;
+	int		j;
+
+	words_count = count_full_words(*ac, *av);
+	split = malloc(sizeof(char *) * words_count);
+	if (!split)
+		return (1);
+	i = 0;
+	j = 0;
+	while (i < words_count)
+	{
+		split[i] = create_words((*av)[])
+	}
+}
+
 t_stacks	*parse(int ac, char **av)
 {
 	t_stacks	*stacks;
 	t_set		*set;
 	int			i;
+	int			n_count;
 
-	if (create_ds(ac, &stacks, &set) == ERROR)
+	if (normalise_args(&ac, &av, &n_count))
 		return (NULL);
+	if (create_ds(n_count, &stacks, &set) == ERROR)
+		return (free_args(av, ac));
 	i = 1;
 	while (i < ac)
 	{

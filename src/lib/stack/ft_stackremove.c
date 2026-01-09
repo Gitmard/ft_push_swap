1bb86bea57fd87ee7d47fa586190ae1fe2462d64
smenard <floogles71@gmail.com>
2026-01-09 11:15:42 +0100

    fix: stackremove when size is 1

--- src/lib/stack/ft_stackremove.c
+++ src/lib/stack/ft_stackremove.c
@@ -6,7 +6,7 @@
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 15:05:32 by smenard           #+#    #+#             */
-/*   Updated: 2026/01/06 17:45:34 by vquetier         ###   ########lyon.fr   */
+/*   Updated: 2026/01/09 11:14:40 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ t_list	*ft_stackremove_head(t_stack *stack)
 
 	old_head = stack->head;
 	stack->head = ft_lstremove_head(stack->head);
+	if (stack->size == 1)
+		stack->tail = NULL;
 	stack->size -= 1;
 	return (old_head);
 }
@@ -28,6 +30,8 @@ t_list	*ft_stackremove_tail(t_stack *stack)
 
 	old_tail = stack->tail;
 	stack->tail = ft_lstremove_tail(stack->tail);
+	if (stack->size == 1)
+		stack->head = NULL;
 	stack->size -= 1;
 	return (old_tail);
 }

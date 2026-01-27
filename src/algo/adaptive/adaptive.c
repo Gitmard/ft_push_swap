e68e514c3afa29011dcea653756e86388f13498e
smenard <floogles71@gmail.com>
2026-01-27 15:27:03 +0100

    feat: adaptive

--- src/algo/adaptive/adaptive.c
+++ src/algo/adaptive/adaptive.c
@@ -1,20 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   algo.h                                             :+:      :+:    :+:   */
+/*   adaptive.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/22 19:31:16 by smenard           #+#    #+#             */
-/*   Updated: 2026/01/27 15:20:04 by smenard          ###   ########.fr       */
+/*   Created: 2026/01/27 14:37:23 by smenard           #+#    #+#             */
+/*   Updated: 2026/01/27 15:27:21 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ALGO_H
-# define ALGO_H
+#include "algo_includes.h"
+#include "algo.h"
 
-# include "simple.h"
-# include "medium.h"
-# include "adaptive.h"
+int	adaptive(t_stacks *stacks, float disorder)
+{
+	int			res;
 
-#endif
+	res = 0;
+	if (disorder <= 0.2)
+		simple(stacks);
+	else if (disorder <= 0.5)
+		res = medium(stacks);
+	else
+	{
+		ft_printf("ERROR: complex not implemented.\n");
+		res = -1;
+	}
+	return (res);
+}

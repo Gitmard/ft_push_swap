e50dd8d0d6e690592e82430f964d2e61fc886e12
smenard <floogles71@gmail.com>
2026-01-28 14:46:50 +0100

    refacto: norm

--- bonus_src/lib/utils/headers/utils_includes_bonus.h
+++ bonus_src/lib/utils/headers/utils_includes_bonus.h
@@ -6,12 +6,12 @@
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 13:29:51 by vquetier          #+#    #+#             */
-/*   Updated: 2026/01/28 13:28:55 by smenard          ###   ########.fr       */
+/*   Updated: 2026/01/28 14:46:23 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef UTILS_INCLUDES_H
-# define UTILS_INCLUDES_H
+#ifndef UTILS_INCLUDES_BONUS_H
+# define UTILS_INCLUDES_BONUS_H
 
 # include "lib_bonus.h"
 

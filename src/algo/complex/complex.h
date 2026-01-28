3c8c1863d42e97c3cf263044b2be857bff665a4c
gossan <quetier.victor.pro@gmail.com>
2026-01-28 14:44:49 +0100

    feat: alog/medium

---
+++ src/algo/complex/complex.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   complex.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vquetier <vquetier@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/28 13:40:24 by vquetier          #+#    #+#             */
+/*   Updated: 2026/01/28 13:41:04 by vquetier         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COMPLEX_H
+# define COMPLEX_H
+
+# include "complex_defines.h"
+
+void	complex(t_stacks *stacks);
+
+#endif

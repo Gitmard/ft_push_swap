354e6eb9463a2c49ee9f659cb0e482e5fccd9ec1
smenard <floogles71@gmail.com>
2026-02-02 16:28:18 +0100

    fix: secure bonus instruction

--- bonus_src/instructions/push_bonus.c
+++ bonus_src/instructions/push_bonus.c
@@ -6,7 +6,7 @@
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 19:32:31 by smenard           #+#    #+#             */
-/*   Updated: 2026/02/02 11:24:17 by vquetier         ###   ########lyon.fr   */
+/*   Updated: 2026/02/02 16:16:52 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 static void	push(t_stack *from, t_stack *to)
 {
+	if (from->size <= 0)
+		return ;
 	ft_stackadd_head(to, ft_stackremove_head(from));
 }
 

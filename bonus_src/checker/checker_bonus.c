a8305504a060cc28efadefb5ecb78b72d979035f
smenard <floogles71@gmail.com>
2026-02-02 17:18:09 +0100

    fix: sigpipe in checker

--- bonus_src/checker/checker_bonus.c
+++ bonus_src/checker/checker_bonus.c
@@ -6,25 +6,21 @@
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 14:00:36 by vquetier          #+#    #+#             */
-/*   Updated: 2026/02/02 16:06:29 by smenard          ###   ########.fr       */
+/*   Updated: 2026/02/02 17:04:31 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker_bonus.h"
-#include <stdio.h>
 
-int	raise_error_checker(int flag)
+int	raise_error_checker()
 {
 	char	buffer[256];
 	int		rd_char;
 
 	write(2, "Error\n", 6);
-	if (flag == DRAIN_STDIN)
-	{
+	rd_char = read(0, buffer, 256);
+	while (rd_char > 0)
 		rd_char = read(0, buffer, 256);
-		while (rd_char > 0)
-			rd_char = read(0, buffer, 256);
-	}
 	return (1);
 }
 
@@ -80,18 +76,17 @@ int	main(int ac, char **av)
 
 	stacks = parse(ac, av);
 	if (!stacks)
-		return (raise_error_checker(NO_DRAIN));
+		return (raise_error_checker());
 	if (stacks->flags != 0)
 	{
 		free_stacks(stacks, FREE_STACKS_ALL);
-		return (raise_error_checker(NO_DRAIN));
+		return (raise_error_checker());
 	}
 	if (handle_operations(stacks, get_op(), get_functions()))
 	{
 		free_stacks(stacks, FREE_STACKS_ALL);
-		return (raise_error_checker(NO_DRAIN));
+		return (raise_error_checker());
 	}
-	printf("WEE WEE\n");
 	if (is_sorted(stacks))
 		write(1, "OK\n", 3);
 	else

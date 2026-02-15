ca6795d774b9451fe836eb420950005154b796c1
Gerard2par2 <floogles71@gmail.com>
2026-02-15 13:16:22 +0100

    fix: checker, parse

--- bonus_src/checker/checker_bonus.h
+++ bonus_src/checker/checker_bonus.h
@@ -6,19 +6,20 @@
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 14:01:04 by vquetier          #+#    #+#             */
-/*   Updated: 2026/02/02 17:04:36 by smenard          ###   ########.fr       */
+/*   Updated: 2026/02/15 13:03:48 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHECKER_BONUS_H
 # define CHECKER_BONUS_H
 
-# include "parse_bonus.h"
 # include "instructions_bonus.h"
 # include "lib_bonus.h"
+# include "parse_bonus.h"
 # include <unistd.h>
 
+void	raise_error_checker(void);
 void	(**get_functions(void))(t_stacks *stacks);
-char	**get_op(void);
+void	clean_exit(t_stacks *stacks, uint32_t flags, int value);
 
 #endif
